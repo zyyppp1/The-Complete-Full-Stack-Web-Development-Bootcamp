6e1004bb1f0c747b6265bf38d0f961be4a054635
zyyppp1 <zyyppp1@gmail.com>
2025-03-31 06:46:50 +0100

    Add functionality for Drum Kit project with sound effects and button animations; update styles and initial JavaScript logic

--- basic.js
+++ basic.js
@@ -1,23 +1,22 @@
-function fibonacciGenerator (n) {
-    //Do NOT change any of the code above 👆
-        
-        //Write your code here:
-        while (n===1){
-            return [0]
-        }   
-        if (n===2){
-            return [0,1]
-        var list=[0,1]
-        while (list.length < n){
-            list.push(list[list.length-1]+list[list.length-2])
-        }
-        return list
-        
-        
-        
-        //Return an array of fibonacci numbers starting from 0.
-        
-    //Do NOT change any of the code below 👇
+
+function add(num1, num2) {
+    return num1 + num2;
     }
-    fibonacciGenerator(10);
-    console.log(fibonacciGenerator(10))
+
+function subtract(num1, num2) {
+    return num1 - num2;
+    }
+     
+function multiply(num1, num2) {
+    return num1 * num2;
+    }
+     
+function divide(num1, num2) {
+    return num1 / num2;
+    }
+     
+function calculator(num1, num2, self) {
+    return self(num1, num2);
+    }
+
+calculator(5, 10, add); // 15
