64438b83d8c467078f6116b6d06059c5909f98ec
zyyppp1 <zyyppp1@gmail.com>
2025-04-10 00:09:56 +0100

    Implement home route and submission handling; render index.ejs with name length feedback

--- 4.2+Passing+Data/index.js
+++ 4.2+Passing+Data/index.js
@@ -6,9 +6,14 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render ("index.ejs", { title: "Home" });
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const namelenght = req.body.fName.length+req.body.lName.length;
+  res.render ("index.ejs", { namelen:namelenght });
+  });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
