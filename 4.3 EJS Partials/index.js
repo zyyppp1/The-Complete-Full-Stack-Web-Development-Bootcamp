fabe58bf95a693baf5e3fba6c9a87c41ad0f0195
zyyppp1 <zyyppp1@gmail.com>
2025-04-12 01:15:23 +0100

    Add .npmignore and .eslintrc files for various node modules; include initial setup for coverage and linting

--- 4.3 EJS Partials/index.js
+++ 4.3 EJS Partials/index.js
@@ -3,12 +3,33 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// Step 2: Make sure that static files are linked to and the CSS shows up.
+
+app.use(express.static("public"))
+
 /* Write your code here:
-Step 1: Render the home page "/" index.ejs
-Step 2: Make sure that static files are linked to and the CSS shows up.
-Step 3: Add the routes to handle the render of the about and contact pages.
-  Hint: Check the nav bar in the header.ejs to see the button hrefs
+
 Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
+
+
+// Step 1: Render the home page "/" index.ejs
+
+app.get("/", (req, res) => {
+  res.render ("index.ejs", { title: "Home" });
+});
+
+// Step 3: Add the routes to handle the render of the about and contact pages.
+//   Hint: Check the nav bar in the header.ejs to see the button hrefs
+
+app.get("/about", (req, res) => {
+  res.render ("about.ejs", { title: "About" });
+});
+app.get("/contact", (req, res) => {
+  res.render ("contact.ejs", { title: "Contact" });
+});
+
+
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
