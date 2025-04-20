b23e2074bfed43673d12fc0b07c525eaf3bdef09
zyyppp1 <zyyppp1@gmail.com>
2025-04-20 21:05:28 +0100

    Add .npmignore and .eslintrc files for various node modules; include initial setup for coverage and linting

--- 5.2 JSON/index.js
+++ 5.2 JSON/index.js
@@ -12,13 +12,32 @@ const recipeJSON =
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let data;
+
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs",{ recipe: data });
 });
 
+
 app.post("/recipe", (req, res) => {
+  switch (req.body.choice) {
+    case "chicken":
+      data = JSON.parse(recipeJSON)[0];
+      break;
+    case "beef":
+      data = JSON.parse(recipeJSON)[1];
+      break;
+    case "fish":
+      data = JSON.parse(recipeJSON)[2];
+      break;
+    default:
+      break;
+  }
   //Step 3: Write your code here to make this behave like the solution website.
   //Step 4: Add code to views/index.ejs to use the recieved recipe object.
+  res.redirect("/");
+
 });
 
 app.listen(port, () => {
