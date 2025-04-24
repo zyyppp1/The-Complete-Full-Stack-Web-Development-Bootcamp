42614f146940cd04e364c8ac1c7ca0f62439e9f4
zyyppp1 <zyyppp1@gmail.com>
2025-04-24 23:52:59 +0100

    Add .npmignore and .eslintignore files for various node modules; include initial setup for coverage and linting

--- 5.3 Axios/index.js
+++ 5.3 Axios/index.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Step 1: Make sure that when a user visits the home page,
 //   it shows a random activity.You will need to check the format of the
 //   JSON data from response.data and edit the index.ejs file accordingly.
+
+
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
